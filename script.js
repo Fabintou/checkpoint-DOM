88c3f6812348c67b40a6261f23998b5f9a398d35
fatou Bintou <fat.binetou2009@gmail.com>
2022-12-21 00:03:09 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,248 @@
+var btPlus = document.getElementById("btPlus");
+var btMoins = document.getElementById("btMoins");
+var nb = document.getElementById("nb");
+var pt = document.getElementById("pt");
+var cpt = parseInt(nb.innerText);
+var Cpt = parseInt(pt.innerText);
+
+
+btPlus.addEventListener("click",function(){
+    cpt = cpt+1;
+    nb.innerText = cpt;
+    pt.innerText = cpt*3000;
+
+});
+btMoins.addEventListener("click",function(){
+    cpt = cpt-1;
+    nb.innerText = cpt; 
+    pt.innerText = cpt*3000;
+   
+});
+
+
+var btPlus2 = document.getElementById("btPlus2");
+var btMoins2 = document.getElementById("btMoins2");
+var nb2 = document.getElementById("nb2");
+var pt2 = document.getElementById("pt2");
+var cpt2 = parseInt(nb2.innerText);
+var Cpt2 = parseInt(pt2.innerText);
+
+
+btPlus2.addEventListener("click",function(){
+    cpt2 = cpt2+1;
+    nb2.innerText = cpt2;
+    pt2.innerText = cpt2*6000;
+
+});
+btMoins2.addEventListener("click",function(){
+    cpt2 = cpt2-1;
+    nb2.innerText = cpt2; 
+    pt2.innerText = cpt2*6000;
+   
+});
+
+
+var btPlus3 = document.getElementById("btPlus3");
+var btMoins3 = document.getElementById("btMoins3");
+var nb3 = document.getElementById("nb3");
+var pt3 = document.getElementById("pt3");
+var cpt3 = parseInt(nb3.innerText);
+var Cpt3 = parseInt(pt3.innerText);
+
+
+btPlus3.addEventListener("click",function(){
+    cpt3 = cpt3+1;
+    nb3.innerText = cpt3;
+    pt3.innerText = cpt3*4000;
+
+});
+btMoins3.addEventListener("click",function(){
+    cpt3 = cpt3-1;
+    nb3.innerText = cpt3; 
+    pt3.innerText = cpt3*4000;
+   
+});
+
+
+
+
+var btPlus4 = document.getElementById("btPlus4");
+var btMoins4 = document.getElementById("btMoins4");
+var nb4 = document.getElementById("nb4");
+var pt4 = document.getElementById("pt4");
+var cpt4 = parseInt(nb4.innerText);
+var Cpt4 = parseInt(pt4.innerText);
+
+
+btPlus4.addEventListener("click",function(){
+    cpt4 = cpt4+1;
+    nb4.innerText = cpt4;
+    pt4.innerText = cpt4*7000;
+
+});
+btMoins4.addEventListener("click",function(){
+    cpt4 = cpt4-1;
+    nb4.innerText = cpt4; 
+    pt4.innerText = cpt4*7000;
+   
+});
+
+
+
+
+var btPlus5 = document.getElementById("btPlus5");
+var btMoins5 = document.getElementById("btMoins5");
+var nb5 = document.getElementById("nb5");
+var pt5 = document.getElementById("pt5");
+var cpt5 = parseInt(nb5.innerText);
+var Cpt5 = parseInt(pt5.innerText);
+
+
+btPlus5.addEventListener("click",function(){
+    cpt5 = cpt5+1;
+    nb5.innerText = cpt5;
+    pt5.innerText = cpt5*1000;
+
+});
+btMoins5.addEventListener("click",function(){
+    cpt5 = cpt5-1;
+    nb5.innerText = cpt5; 
+    pt5.innerText = cpt5*1000;
+   
+});
+
+
+
+
+var btPlus6 = document.getElementById("btPlus6");
+var btMoins6 = document.getElementById("btMoins6");
+var nb6 = document.getElementById("nb6");
+var pt6 = document.getElementById("pt6");
+var cpt6 = parseInt(nb6.innerText);
+var Cpt6 = parseInt(pt6.innerText);
+
+
+btPlus6.addEventListener("click",function(){
+    cpt6 = cpt6+1;
+    nb6.innerText = cpt6;
+    pt6.innerText = cpt6*3000;
+
+});
+btMoins6.addEventListener("click",function(){
+    cpt6 = cpt6-1;
+    nb6.innerText = cpt6; 
+    pt6.innerText = cpt6*3000;
+   
+});
+
+
+
+
+var btPlus7 = document.getElementById("btPlus7");
+var btMoins7 = document.getElementById("btMoins7");
+var nb7 = document.getElementById("nb7");
+var pt7 = document.getElementById("pt7");
+var cpt7 = parseInt(nb7.innerText);
+var Cpt7 = parseInt(pt7.innerText);
+
+
+btPlus7.addEventListener("click",function(){
+    cpt7 = cpt7+1;
+    nb7.innerText = cpt7;
+    pt7.innerText = cpt7*12000;
+
+});
+btMoins7.addEventListener("click",function(){
+    cpt7 = cpt7-1;
+    nb7.innerText = cpt7; 
+    pt7.innerText = cpt7*12000;
+   
+});
+
+
+var btPlus8 = document.getElementById("btPlus8");
+var btMoins8 = document.getElementById("btMoins8");
+var nb8 = document.getElementById("nb8");
+var pt8 = document.getElementById("pt8");
+var cpt8 = parseInt(nb8.innerText);
+var Cpt8 = parseInt(pt8.innerText);
+
+
+btPlus8.addEventListener("click",function(){
+    cpt8 = cpt8+1;
+    nb8.innerText = cpt8;
+    pt8.innerText = cpt8*5000;
+
+});
+btMoins8.addEventListener("click",function(){
+    cpt8 = cpt8-1;
+    nb8.innerText = cpt8; 
+    pt8.innerText = cpt8*5000;
+   
+});
+
+var btPlus8 = document.getElementById("btPlus8");
+var btMoins8 = document.getElementById("btMoins8");
+var nb8 = document.getElementById("nb8");
+var pt8 = document.getElementById("pt8");
+var cpt8 = parseInt(nb8.innerText);
+var Cpt8 = parseInt(pt8.innerText);
+
+
+btPlus8.addEventListener("click",function(){
+    cpt8 = cpt8+1;
+    nb8.innerText = cpt8;
+    pt8.innerText = cpt8*5000;
+
+});
+
+
+document.getElementById("btn1").onclick = function() {
+    document.getElementById("output").style.visibility = "hidden";
+  }
+  document.getElementById("btn2").onclick = function() {
+    document.getElementById("output2").style.visibility = "hidden";
+  }
+
+document.getElementById("btn3").onclick = function() {
+    document.getElementById("output3").style.visibility = "hidden";
+  }
+
+  document.getElementById("btn4").onclick = function() {
+    document.getElementById("output4").style.visibility = "hidden";
+  }
+
+  document.getElementById("btn5").onclick = function() {
+    document.getElementById("output5").style.visibility = "hidden";
+  }
+
+  document.getElementById("btn6").onclick = function() {
+    document.getElementById("output6").style.visibility = "hidden";
+  }
+document.getElementById("btn7").onclick = function() {
+    document.getElementById("output7").style.visibility = "hidden";
+  }
+
+  document.getElementById("btn7").onclick = function() {
+    document.getElementById("output7").style.visibility = "hidden";
+  }
+  
+  document.getElementById("btn8").onclick = function() {
+    document.getElementById("output8").style.visibility = "hidden";
+  }
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+ 
+      
+ 
